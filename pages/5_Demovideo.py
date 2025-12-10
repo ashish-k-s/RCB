02e4159b2eae2408769705d613d33c28a516f71e
Ashish shah <ashishks@redhat.com>
2025-12-10 13:45:13 +0530

    Code to Trim the selected video at provided timestamps in Actions

--- pages/5_Demovideo.py
+++ pages/5_Demovideo.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 import subprocess
 
 from rcb_init import init_page
+from moviepy import VideoFileClip, concatenate_videoclips
 
 st.title("Build Your Demovideo with RCB")
 if 'current_page' not in st.session_state:
@@ -16,24 +17,43 @@ st.session_state.current_page = "DemoVideo"
 init_page()
 
 if "selected_file" not in st.session_state:
-    st.session_state.selected_file = ""
+    st.session_state.selected_file_name = ""
 if "video_path" not in st.session_state:
-    st.session_state.video_path = ""
-if "current_time" not in st.session_state:
-    st.session_state.current_time = ""
+    st.session_state.selected_file_path = ""
+if "current_timestamp" not in st.session_state:
+    st.session_state.current_timestamp = ""
 if "action_str" not in st.session_state:
-    st.session_state.action_str = ""
+    st.session_state.action_str = "Trim"
 if "action_text" not in st.session_state:
-    st.session_state.action_text = ""
-if "dub_disabled" not in st.session_state:
-    st.session_state.dub_disabled = False
-if "cut_disabled" not in st.session_state:
-    st.session_state.cut_disabled = False
+    st.session_state.action_text = "Trim"
+if 'selected_index' not in st.session_state:
+    st.session_state.selected_index = 0
+if 'trim_selected' not in st.session_state:
+    st.session_state.trim_selected = False
+if 'dub_selected' not in st.session_state:
+    st.session_state.dub_selected = False
+if 'speed_selected' in st.session_state:
+    st.session_state.speed_disabled = False
+
+def set_action(selected_option):
+    # if st.session_state.action_text != "":
+    #     if not clear_actions():
+    #         return
+    st.session_state.action_str = selected_option
+    if st.session_state.action_str == "Trim":
+        st.session_state.selected_index = 0
+    elif st.session_state.action_str == "Speed":
+        st.session_state.selected_index = 1
+    elif st.session_state.action_str == "Dub":
+        st.session_state.selected_index = 2
+    st.session_state.trim_selected = (selected_option == "Trim")
+    st.session_state.speed_selected = (selected_option == "Speed")
+    st.session_state.dub_selected = (selected_option == "Dub")
 
 # Display the selected video with JavaScript and other streamlit controls
-## if st.session_state.video_path and os.path.exists(st.session_state.video_path):
+## if st.session_state.selected_file_path and os.path.exists(st.session_state.selected_file_path):
 try:
-    video_files = [f for f in os.listdir(st.session_state.user_dir) if f.endswith(".webm")]
+    video_files = [f for f in os.listdir(f"{st.session_state.user_dir}/saved_videos") if f.endswith(".webm")]
 except FileNotFoundError:
     st.sidebar.info("No demo video found. Please upload a .webm video file.")
 
@@ -46,12 +66,32 @@ def update_action(action_str=""):
 def clear_actions():
     if st.button("Yes"):
         st.session_state.num_actions = 1
-        st.session_state.current_time = ""
+        st.session_state.current_timestamp = ""
         st.session_state.action_str = ""
         st.session_state.action_text = ""
         st.rerun()
     else:
-        pass
+        return False
+
+def display_trim_options():
+    pass
+def display_speed_options():
+    pass
+def display_dub_options():
+    pass
+
+def remove_multiple_sections(video_path, cuts, output_path):
+    clip = VideoFileClip(video_path)
+    clips = []
+    last = 0
+
+    for start, end in cuts:
+        clips.append(clip.subclipped(last, start))
+        last = end
+
+    clips.append(clip.subclipped(last, clip.duration))  # final segment
+    final = concatenate_videoclips(clips)
+    final.write_videofile(output_path)
 
 def seconds_to_hhmmss_timedelta(seconds):
     """Converts seconds to hh:mm:ss format using timedelta, discarding fractions."""
@@ -63,12 +103,12 @@ if not st.session_state.username:
     st.stop()
     
 if video_files:
-    st.sidebar.subheader("Select a demo video from your uploads:")
-    st.session_state.selected_file = st.sidebar.selectbox("Choose a file:", video_files)
-    st.session_state.video_path = os.path.join(st.session_state.user_dir, st.session_state.selected_file)
-    st.sidebar.info(f"Selected file: {st.session_state.selected_file}")
+    st.sidebar.subheader("Select a video file to edit:")
+    st.session_state.selected_file_name = st.sidebar.selectbox("Choose a file:", video_files)
+    st.session_state.selected_file_path = os.path.join(f"{st.session_state.user_dir}/saved_videos", st.session_state.selected_file_name)
+    st.sidebar.info(f"Selected file: {st.session_state.selected_file_name}")
 
-    with open(st.session_state.video_path, "rb") as f:
+    with open(st.session_state.selected_file_path, "rb") as f:
         video_bytes = f.read()
 
     b64_video = base64.b64encode(video_bytes).decode()
@@ -109,104 +149,79 @@ if video_files:
 
     code = html_code + js_code
 
-    st.subheader(f"Selected Demo Video: {st.session_state.selected_file}")
+    st.subheader(f"Selected Demo Video: {st.session_state.selected_file_name}")
     components.html(code, height=400, width=700, scrolling=True,)
     value = bridge("currentTime", default="")
-    st.session_state.current_time = value
-    ## st.session_state["echo_value"] = st.session_state.current_time
+    st.session_state.current_timestamp = value
+    ## st.session_state["echo_value"] = st.session_state.current_timestamp
     ## st.text_input("Streamlit input (from JS)", key="echo_value")
-    ## st.session_state.action_text += st.session_state.current_time
+    ## st.session_state.action_text += st.session_state.current_timestamp
 
     if "num_actions" not in st.session_state:
         st.session_state.num_actions = 1  # start with one
 
     def add_action():
         st.session_state.num_actions += 1
+        update_action("\n")
 
     # Button to add new text action
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        cut = st.button("cut",disabled=st.session_state.cut_disabled)
-        dub = st.button("dub",disabled=st.session_state.dub_disabled)
-
+        options = ["Trim", "Speed", "Dub"]
+        action = st.selectbox("Action Type:", options, index=st.session_state.selected_index, disabled=st.session_state.get("disable_all"))
+        set_action(action)
     with col2:
-        time = st.button("time")
-        add = st.button("Add action", on_click=add_action)
+        if st.session_state.trim_selected:
+            display_trim_options()
+        elif st.session_state.speed_selected:
+            display_speed_options()
+        elif st.session_state.dub_selected:
+            display_dub_options()
+        get_timestamp = st.button("Get Current timestamp")
+        add_action = st.button("Add action", on_click=add_action)
 
-    with col3:
-        get_audio_file = st.button("audio file",key="get_audio_file",disabled=st.session_state.get("cut_disabled"))
-        audio_file = st.selectbox("Select Audio:", options=["audio1.mp3", "audio2.mp3", "audio3.mp3"], label_visibility="collapsed",key="audio_file",disabled=st.session_state.get("cut_disabled"))
-
-    if cut:
-        proceed = True
-        if st.session_state.action_text:
-            st.toast("Clear all actions before switching mode.")
-            proceed = False
-        if proceed:
-            ##update_action("cut:\n")
-            st.session_state.dub_disabled = False
-            st.session_state.cut_disabled = True
-            st.rerun()
-
-    if dub:
-        proceed = True
-        if st.session_state.action_text:
-            st.toast("Clear all actions before switching mode.")
-            proceed = False
-        if proceed:
-            ##update_action("dub:\n")
-            st.session_state.cut_disabled = False
-            st.session_state.dub_disabled = True
-            st.rerun()
-
-    if add:
-        update_action("\n")
-
-    if time:
+    if get_timestamp:
         try:
-            st.session_state.current_time = seconds_to_hhmmss_timedelta(float(st.session_state.current_time.strip()))
+            st.session_state.current_timestamp = seconds_to_hhmmss_timedelta(float(st.session_state.current_timestamp.strip()))
         except ValueError:
-            st.session_state.current_time = "00:00"
-        update_action(st.session_state.current_time + " ")
+            st.session_state.current_timestamp = "00:00"
+        update_action(st.session_state.current_timestamp + " ")
 
-    if get_audio_file:
-        update_action(f"{audio_file} ")
+
+    # with col3:
+    #     get_audio_file = st.button("audio file",key="get_audio_file",disabled=st.session_state.get("cut_disabled"))
+    #     audio_file = st.selectbox("Select Audio:", options=["audio1.mp3", "audio2.mp3", "audio3.mp3"], label_visibility="collapsed",key="audio_file",disabled=st.session_state.get("cut_disabled"))
 
     # Render the text actions
-    st.text_area("Actions:", placeholder="Enter text for actions here...", height=300, value=st.session_state.action_text)
+    st.session_state.action_text = st.text_area("Actions:", placeholder="Enter text for actions here...", height=300, value=st.session_state.action_text)
 
     if st.button("Clear all Actions"):
         clear_actions()
 
-    col1, col2, col3 = st.columns(3)
+ 
+    col1, col2 = st.columns(2)
 
-    with col1:
-        st.radio("Video Processing Mode:", options=["Fast", "Precise"], index=0)
-
-    with col2:    
-        video_file_name = st.text_input("Video File Name")
+    with col1:    
+        generate_video_file_name = st.text_input("Video File Name")
     
-    with col3:
+    with col2:
         empty = st.empty()
-        generate_video = st.button("Generate Video",disabled=not video_file_name)
+        generate_video = st.button("Generate Video",disabled=not generate_video_file_name)
     
-    if generate_video and video_file_name:
-        print(f"Generating video file: {video_file_name}.webm")
+    if generate_video and generate_video_file_name:
+        print(f"Generating video file: {generate_video_file_name}.webm")
+        generate_video_file_path = os.path.join(f"{st.session_state.user_dir}/saved_videos", generate_video_file_name + ".webm")
+
         if st.session_state.action_text:
-            try:
-                with open("trim.txt", "w") as f:
-                    f.write(st.session_state.action_text)
-                print("Actions written to trim.txt:")
-                print(st.session_state.action_text)
-            except Exception as e:
-                print(f"Error writing actions to file: {e}")
-        print(f"Calling trim.sh with: {st.session_state.user_dir}/{st.session_state.selected_file}")
-        subprocess.run(f"sh /home/ashishks/AI/UI/new/RCB/trim.sh {st.session_state.user_dir}/{st.session_state.selected_file}", shell=True)
-        print(f"Video file {video_file_name}.webm generated successfully!")
+            if st.session_state.action_str == "Trim":
+                timestamps_to_trim = [tuple(line.split()) for line in st.session_state.action_text.splitlines() if line.strip()]
 
-        # components.html("<script>document.getElementById('timestampBox').value = '';</script>", height=0)
+                print(timestamps_to_trim)
 
+                remove_multiple_sections(st.session_state.selected_file_path, timestamps_to_trim, generate_video_file_path)
+                st.success(f"Video generated and saved as {generate_video_file_name}.webm")
+                
 else:
     st.sidebar.info("No demo video found. Please upload a .webm video file.")
 
@@ -224,7 +239,7 @@ if uploaded_file:
     if process_file_button:
         print(f"UPLOADED FILES:\n {uploaded_file}")
         # Process the uploaded video file
-        st.session_state.video_path = os.path.join(st.session_state.user_dir, uploaded_file.name)
-        with open(st.session_state.video_path, "wb") as f:
+        st.session_state.selected_file_path = os.path.join(f"{st.session_state.user_dir}/saved_videos", uploaded_file.name)
+        with open(st.session_state.selected_file_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
         st.rerun()
